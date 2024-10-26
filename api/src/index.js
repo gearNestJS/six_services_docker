c621e70424c432b86ef58298dbbd5f534f51768b
Valeriy Semenenko <gearmobile@gmail.com>
2024-10-26 22:36:05 +0300

    feat: :sparkles: add api service

--- api/src/index.js
+++ api/src/index.js
@@ -3,10 +3,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (_, res) => {
+    res.redirect('test');
+});
+
 app.get('/test', (_, res) => {
     res.send('Test response working fine!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Api listening at http://localhost:${PORT}`);
+app.listen(PORT, (_) => {
+    console.log(`Api service listening at http://localhost:${PORT}`);
 })

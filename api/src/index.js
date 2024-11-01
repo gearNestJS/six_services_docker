292df77263d01b43badd7a90fdd5627cfdb4df5d
Valeriy Semenenko <gearmobile@gmail.com>
2024-11-01 23:16:38 +0300

    feat: :sparkles: test mongo db connection

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const { connectDB } = require('../helpers/db');
 const { PORT, HOST } = require('../configurations');
 
+const postSchema = new mongoose.Schema({
+    name: String
+});
+
+const Post = mongoose.model('Post', postSchema);
+
 const startServer = () => {
     app.listen(PORT, (_) => {
         console.log(`Api service listening at ${HOST}:${PORT}`);
+        const post = new Post({ name: 'test' });
+        post.save().then((post) => console.log('Post saved!', post));
+        Post.find().then((posts) => console.log('Posts found!', posts));
     });
 };
 
